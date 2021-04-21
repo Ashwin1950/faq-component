22cd79cbe3784d15324ff8967b6f11076053dc66
Ashwin Muthu Kumar S <67495595+Ashwin1950@users.noreply.github.com>
2021-04-21 13:34:13 +0530

    modified faq

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -20,15 +20,18 @@
               </summary>
 
               <span class="mt-2 ml-4">
-                The title is intentionally misspelled, as it also appears as
-                <b>graffiti</b> in a scene in the film. The misspelled phrase is
-                actually taken from an essay written in <b>1776</b> that argued
-                that whites and blacks were created equal. The essay, which was
-                written by <b>Lemuel Haynes</b>, a biracial man living in New
-                England during the Revolution, quoted Thomas Jefferson's
-                well-known sentence from the
-                <b>United States Declaration of Independence</b>, but spelled
-                the last word of the sentence with a <b>y</b>.
+                The word <b>happiness</b> in the title is <b>deliberately misspelled</b>,
+                just as it was on the wall of a day care center where Gardner
+                once sought care for his young son, Chris Jr., during some of
+                his worst days." Chris (Will Smith) even points out the
+                misspelling to the Chinese care-giver that the mural decorating
+                his son's daycare is spelled incorrectly. Chris wanted to make
+                sure that his son was taught things the correct way, so that he
+                could grow up to be an educated man and make something great of
+                himself. Chris <b>took offense</b> to the fact that the world
+                happiness was spelled wrong on the wall and became irritated by
+                it. <b>But the word comes to signify Chris' own pursuit of
+                <span class="line-through">happiness</span> happyness</b>.
               </span>
             </details>
             <details class="mb-4">
@@ -47,6 +50,9 @@
                 serves as a prime example of
                 <b>how we must overcome difficulties, not be overcome by them</b
                 >.
+                <img
+                  src="https://qphs.fs.quoracdn.net/main-qimg-adec548f5ab584a1a6c0d408e9cb8b58"
+                />
               </span>
             </details>
             <details class="mb-4">
@@ -66,23 +72,42 @@
                   >Christopher Gardner and his son Christopher Jarrett Medina
                   Gardner</b
                 >.
+                <img src="https://hips.hearstapps.com/hbz.h-cdn.co/assets/17/15/1492024778-hbz-will-jaden-smith-index.jpg" alt="Will Smith and Jaden Smith - Father and Son">
               </span>
             </details>
-            <details>
+            <details class="mb-4">
               <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-1">
                 Did the wife of Gardner ever return?
               </summary>
 
               <span class="mt-2 ml-4">
-                <b>No</b>, she didn't return. <br>I think it was suggested from the plot
-                that there came a point of <b>no-return</b> where she couldn't take it
-                anymore and moved on. She had also left his son behind with
-                Gardner and that was quite surprising as she had a job unlike
-                her husband. She knew it very well that she was in a position to
-                give their son a better life as compared to her husband at that
-                moment of time. It was like leaving everything associated with
-                him whether it was her own son. She <b>moved on with her life</b> and
-                Gardner has also accepted that fact.
+                <b>No</b>, she didn't return. <br />I think it was suggested
+                from the plot that there came a point of <b>no-return</b> where
+                she couldn't take it anymore and moved on. She had also left his
+                son behind with Gardner and that was quite surprising as she had
+                a job unlike her husband. She knew it very well that she was in
+                a position to give their son a better life as compared to her
+                husband at that moment of time. It was like leaving everything
+                associated with him whether it was her own son. She
+                <b>moved on with her life</b> and Gardner has also accepted that
+                fact.
+              </span>
+            </details>
+            <details>
+              <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-1">
+                What is the Timeline of the movie?
+              </summary>
+
+              <span class="mt-2 ml-4">
+                <ul style="list-style-type:disc;" class="ml-10">
+                  <li><b>1981</b> - Chris Gardner invests in scanners</li>
+                  <li><b>Jan 1, 1982</b> - Chris Gardner sends his application to Jay Twistle</li>
+                  <li><b>Feb 2, 1982</b> - Chris Gardner talks to Jay Twistle and solves a rubiks cube in a Taxi</li>
+                  <li><b>Mar 3, 1982</b> - Linda leaves Chris</li>
+                  <li><b>May 5, 1982</b> - Chris goes to jail</li>
+                  <li><b>Jun 6, 1982</b> - Chris gets invited to the internship</li>
+                  <li><b>Jul 7, 1983</b> - Chris completes the internship</li>
+                </ul>
               </span>
             </details>
           </div>
@@ -105,6 +130,7 @@
                 refers to <b>“Marc Rich”</b>, who Gardner considered being one
                 of the most successful futures traders in history. The man
                 wasn’t associated with the company in any way though.
+                <img src="https://www.5-sterne-redner.de/typo3temp/GB/9e867b588c.jpg" alt="Chris Gardner speaking on stage">
               </span>
             </details>
             <details class="mb-4 ml-3 mr-3">
@@ -128,6 +154,7 @@
                 Gardner walks confidently past Will Smith and Jaden. Will Smith
                 briefly looks back and smiles at him as he and his son leisurely
                 stroll around the streets of San Francisco.
+                <img src="https://i.redd.it/ss45k2b5m8l41.jpg" alt="Chris Gardner cameo in The Pursuit of Happyness">
               </span>
             </details>
             <details class="mb-4 ml-3 mr-3">
@@ -137,13 +164,29 @@
 
               <span class="px-1 py-4 ml-4">
                 This movie has taught me many life lessons. Some are:-
-                <ul>
-                  <li>Believe in yourself even if others don't believe in you.</li>
+                <ul style="list-style-type:square;" class="ml-10">
+                  <li>
+                    Believe in yourself even if others don't believe in you.
+                  </li>
                   <li>Set-backs should not stop you from rising.</li>
                   <li>Never give up and keep trying.</li>
-                  <li>Importance of RELATIONSHIPS. A beautiful father and son relationship is shown in the movie.</li>
+                  <li>
+                    Importance of RELATIONSHIPS. A beautiful father and son
+                    relationship is shown in the movie.
+                  </li>
                   <li>DREAM.</li>
                 </ul>
+              </span>
+            </details>
+            <details class="mb-4 ml-3 mr-3">
+              <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-1">
+                Where can I watch "The Pursuit of Happyness"?
+              </summary>
+              <span class="px-1 py-4 ml-4">
+                You can watch the movie in:
+                <ol style="list-style-type:circle;" class="ml-10">
+                <li><a class="text-blue-700 hover:text-red-500 hover:underline" href="https://www.primevideo.com/detail/0MW4Y2G0VYCAN9FD9NU3J3RA0U/ref=atv_dl_rdr">Amazon Prime Video</a></li>
+                </ol>
               </span>
             </details>
           </div>
